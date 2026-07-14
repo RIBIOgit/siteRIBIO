510db6d937e0416b0dc16d6c11287b9b7bcb15e4
RIBIO <redeintegradadebiosseguranca@gmail.com>
2026-07-14 11:07:01 -0300

    Texto genérico falando a mudança

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 python manage.py migrate
+python manage.py collectstatic --noinput
 python manage.py createsuperuser --noinput || true
 gunicorn core.wsgi:application --bind 0.0.0.0:8000
